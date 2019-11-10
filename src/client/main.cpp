fda05315c0bd0e16012be985fec3e6aa21700e94
Robin <sicsic.robin@gmail.com>
2019-11-10 15:32:30 +0100

    Add mouse coordinates in main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -13,17 +13,14 @@ using namespace sf;
 using namespace render;
 using namespace engine;
 
-void testSFML() {
-    Texture texture;
-}
-
 int main(int argc,char* argv[1])
 {
     if(argc>=2 && string(argv[1])=="hello") {
         cout << "Hello world !" << endl;
     } else if (argc>=2 && string(argv[1])=="renderTest1"){
-        cout << "No test renderTest1: try renderTest2." << endl;
+        cout << "La commande renderTest1 est obsolète." << endl;
     } else if (argc>=2 && string(argv[1])=="renderTest2"){
+          cout<<"La commande renderTest2 est obsolète."<<endl;
           cout<<"--- Affichage de l'état initial de jeu ---"<<endl;
 
     			// -- Initialisation de l'état intial de jeu --
@@ -33,28 +30,30 @@ int main(int argc,char* argv[1])
 
     			// -- Affichage de cet Etat --
     			RenderLayer stateLayer(initialState, window);
-          
+
           stateLayer.draw(window);
 
-
-
     } else if (argc>=2 && string(argv[1])=="Engine") {
-      cout<<"Moteur"<<endl;
+      cout<<"Lancement de la commande Engine."<<endl;
       Engine engine;
       sf::RenderWindow window(sf::VideoMode(876,949), "Jungle War");
+      int renderWidth = VideoMode::getDesktopMode().width;
+      int renderHeight = VideoMode::getDesktopMode().height;
+      //sf::RenderWindow window(sf::VideoMode(renderWidth,renderHeight), "Jungle War");
+      window.setFramerateLimit(60);
       RenderLayer stateLayer(engine.getState(), window);
       RenderLayer* ptr_stateLayer=&stateLayer;
       engine.getState().registerObserver(ptr_stateLayer);
 
       bool demarrage = true;
-
       while (window.isOpen()){
         sf::Event event;
-
+        int mouseX = sf::Mouse::getPosition(window).x;
+        int mouseY = sf::Mouse::getPosition(window).y;
+        //cout << mouseX << " : "<< mouseY << endl;
         if (demarrage){
           stateLayer.draw(window);
-          cout << "(Appuyez sur une touche pour simuler un tour de jeu)" << endl;
-          cout << "(Cette simulation compte 3 tours)\n" << endl;
+          cout << "(Cliquez pour simuler un tour de jeu)" << endl << endl;
           demarrage = false;
         }
 
@@ -63,12 +62,15 @@ int main(int argc,char* argv[1])
             window.close();
           }
           else if(event.type == sf::Event::MouseButtonPressed ){
-            cout <<"touche!"<<endl;
+            cout << "Mouse clic event : " << mouseX << " , "<< mouseY << endl;
             Animal &rat = engine.getState().getPlayer1().getAnimals().at(RAT);
             Move move1(rat,Coord(5,5));
             move1.execute(engine.getState());
             StateEvent stateEvent(ALL_CHANGED);
             engine.getState().notifyObservers(stateEvent,engine.getState());
+            cout << "End of the move." << endl << endl;
+
+
             /*int ligne = 2;
 
             while(rat.getCoord() != Coord(1,5)){
