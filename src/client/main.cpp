9d0e01dc74c042aecc2924671777920130658487
Robin <sicsic.robin@gmail.com>
2019-11-10 16:07:27 +0100

    Add condition in main: need to clic on the RAT to move the RAT

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -13,85 +13,82 @@ using namespace sf;
 using namespace render;
 using namespace engine;
 
-int main(int argc,char* argv[1])
-{
-    if(argc>=2 && string(argv[1])=="hello") {
-        cout << "Hello world !" << endl;
-    } else if (argc>=2 && string(argv[1])=="renderTest1"){
-        cout << "La commande renderTest1 est obsolète." << endl;
-    } else if (argc>=2 && string(argv[1])=="renderTest2"){
-          cout<<"La commande renderTest2 est obsolète."<<endl;
-          cout<<"--- Affichage de l'état initial de jeu ---"<<endl;
+int main(int argc,char* argv[1]) {
+  if(argc>=2 && string(argv[1])=="hello") {
+      cout << "Hello world !" << endl;
+  } else if (argc>=2 && string(argv[1])=="renderTest1"){
+      cout << "La commande renderTest1 est obsolète." << endl;
+  } else if (argc>=2 && string(argv[1])=="renderTest2"){
+        cout<<"La commande renderTest2 est obsolète."<<endl;
+        cout<<"--- Affichage de l'état initial de jeu ---"<<endl;
 
-    			// -- Initialisation de l'état intial de jeu --
-    			State initialState("Robin","Baptiste");
+  			// -- Initialisation de l'état intial de jeu --
+  			State initialState("Robin","Baptiste");
 
-    			sf::RenderWindow window(sf::VideoMode(876,949), "Jungle War");
+  			sf::RenderWindow window(sf::VideoMode(876,949), "Jungle War");
 
-    			// -- Affichage de cet Etat --
-    			RenderLayer stateLayer(initialState, window);
+  			// -- Affichage de cet Etat --
+  			RenderLayer stateLayer(initialState, window);
 
-          stateLayer.draw(window);
+        stateLayer.draw(window);
 
-    } else if (argc>=2 && string(argv[1])=="Engine") {
-      cout<<"Lancement de la commande Engine."<<endl;
-      Engine engine;
-      sf::RenderWindow window(sf::VideoMode(876,949), "Jungle War");
-      int renderWidth = VideoMode::getDesktopMode().width;
-      int renderHeight = VideoMode::getDesktopMode().height;
-      //sf::RenderWindow window(sf::VideoMode(renderWidth,renderHeight), "Jungle War");
-      window.setFramerateLimit(60);
-      RenderLayer stateLayer(engine.getState(), window);
-      RenderLayer* ptr_stateLayer=&stateLayer;
-      engine.getState().registerObserver(ptr_stateLayer);
+  } else if (argc>=2 && string(argv[1])=="Engine") {
+    cout<<"Lancement de la commande Engine."<<endl;
+    Engine engine;
+    sf::RenderWindow window(sf::VideoMode(876,949), "Jungle War");
+    // FULL SCREEN
+    /*int renderWidth = VideoMode::getDesktopMode().width;
+    int renderHeight = VideoMode::getDesktopMode().height;
+    sf::RenderWindow window(sf::VideoMode(renderWidth,renderHeight), "Jungle War");*/
+    window.setFramerateLimit(60);
+    RenderLayer stateLayer(engine.getState(), window);
+    RenderLayer* ptr_stateLayer=&stateLayer;
+    engine.getState().registerObserver(ptr_stateLayer);
 
-      bool demarrage = true;
-      while (window.isOpen()){
-        sf::Event event;
-        int mouseX = sf::Mouse::getPosition(window).x;
-        int mouseY = sf::Mouse::getPosition(window).y;
-        //cout << mouseX << " : "<< mouseY << endl;
-        if (demarrage){
-          stateLayer.draw(window);
-          cout << "(Cliquez pour simuler un tour de jeu)" << endl << endl;
-          demarrage = false;
+    bool demarrage = true;
+    while (window.isOpen()){
+      sf::Event event;
+      int mouseX = Mouse::getPosition(window).x;
+      int mouseY = Mouse::getPosition(window).y;
+      int mouseGridX = mouseX/73;
+      int mouseGridY = mouseY/73;
+      //cout << mouseX << " : "<< mouseY << endl;
+      if (demarrage){
+        stateLayer.draw(window);
+        cout << "(Cliquez pour simuler un tour de jeu)" << endl << endl;
+        demarrage = false;
+      }
+
+      while (window.pollEvent(event)){
+        if (event.type == Event::Closed){
+          window.close();
         }
-
-        while (window.pollEvent(event)){
-          if (event.type == sf::Event::Closed){
-            window.close();
-          }
-          else if(event.type == sf::Event::MouseButtonPressed ){
-            cout << "Mouse clic event : " << mouseX << " , "<< mouseY << endl;
-            Animal &rat = engine.getState().getPlayer1().getAnimals().at(RAT);
+        else if(event.type == Event::MouseButtonPressed) {
+          cout << "Mouse clic wind event : " << mouseX << " , "<< mouseY << endl;
+          cout << "Mouse clic grid event : " << mouseGridX << " , "<< mouseGridY << endl << endl;
+          Animal &rat = engine.getState().getPlayer1().getAnimals().at(RAT);
+          if (mouseGridX == rat.getCoord().getX()) {
+            cout << "-- Beginning of the move --" << endl;
             Move move1(rat,Coord(5,5));
             move1.execute(engine.getState());
             StateEvent stateEvent(ALL_CHANGED);
             engine.getState().notifyObservers(stateEvent,engine.getState());
-            cout << "End of the move." << endl << endl;
+            cout << "-- End of the move --" << endl << endl;
+          }
+          /*int ligne = 2;
 
-
-            /*int ligne = 2;
-
-            while(rat.getCoord() != Coord(1,5)){
-              Move move(rat,Coord(1,ligne));
-              move.execute(engine.getState());
-              StateEvent stateEvent(ALL_CHANGED);
-              engine.getState().notifyObservers(stateEvent,engine.getState());
-              ligne = ligne + 1;
-              sleep(1);*/
-
-
+          while(rat.getCoord() != Coord(1,5)){
+            Move move(rat,Coord(1,ligne));
+            move.execute(engine.getState());
+            StateEvent stateEvent(ALL_CHANGED);
+            engine.getState().notifyObservers(stateEvent,engine.getState());
+            ligne = ligne + 1;
+            sleep(1);*/
+          }
+        }
       }
+    } else {
+      cout << "Veuillez dire une commande (hello, renderTest1, renderTest2) !" << endl;
     }
-
-
-
-
-
-    }} else {
-        cout << "Veuillez dire une commande (hello, renderTest1, renderTest2) !" << endl;
-    }
-    return 0;
-
+  return 0;
 }
