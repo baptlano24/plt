8c3628e4d53651664562f67c8b90636368da75f8
Lanoue <baptiste.lanoue@ensea.fr>
2019-11-12 15:07:37 +0100

    Add ID element in Animal

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -30,20 +30,36 @@ void Engine::addOrder (int priorite, std::unique_ptr<Order> ptr_cmd){
 	currentOrder[priorite]=move(ptr_cmd);
 }
 
-std::unordered_map<engine::ActionID,state::Coord>& authorisedActions (state::State& state, state::AnimalID AnimalID){
-  if (AnimalID == ELEPHANT or CAT or DOG or WOLF){
-    if(state.getPlayer1().getAnimals().at(AnimalID).getStatus() == NORMAL){
-        Coord Case_actuel = state.getPlayer1().getAnimals().at(AnimalID).getCoord();
-        Coord Case_droite = {Case_actuel.getX()+1,Case_actuel.getY()};
-        Coord Case_gauche = {Case_actuel.getX()-1,Case_actuel.getY()};
-        Coord Case_devant = {Case_actuel.getX(),Case_actuel.getY()-1};
-        Coord Case_derriere = {Case_actuel.getX(),Case_actuel.getY()+1};
-        if(state.getSquare(Case_droite).getID()!=WATER || NOMAP){
+std::vector<pair<state::Coord, engine::ActionID>> authorisedActions (state::State& state, state::AnimalID animalID){
+  std::vector<pair<state::Coord,engine::ActionID>> listAction;
+  Coord Case_actuel = state.getPlayer1().getAnimals().at(animalID).getCoord();
+  Coord Case_droite = {Case_actuel.getX()+1,Case_actuel.getY()};
+  Coord Case_gauche = {Case_actuel.getX()-1,Case_actuel.getY()};
+  Coord Case_devant = {Case_actuel.getX(),Case_actuel.getY()-1};
+  Coord Case_derriere = {Case_actuel.getX(),Case_actuel.getY()+1};
+  listAction.push_back(make_pair(Case_droite,NONE));
+  listAction.push_back(make_pair(Case_gauche,NONE));
+  listAction.push_back(make_pair(Case_devant,NONE));
+  listAction.push_back(make_pair(Case_derriere,NONE));
+  if (animalID == ELEPHANT || animalID ==  CAT || animalID == DOG || animalID == WOLF){
+    if(state.getPlayer1().getAnimals().at(animalID).getStatus() == NORMAL){
+      for (int i ; i<=4 ;i++) {
+        if(state.getSquare(listAction[i].first).getID()!=WATER){
+          if(state.getSelection(listAction[i].first).second){
+              
+            //if(animalID>state.getSelection(listAction[i].first).first.getID()){
+              //listAction[i].second = ATTACK;
+            }
+            //else{
+              //listAction[i].second = NONE;
+            //}
 
 
+          }
         }
       }
-
     }
+
+
 
   }
