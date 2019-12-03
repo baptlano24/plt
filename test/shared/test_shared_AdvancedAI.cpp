136dcb552046f2f563dc7087ca4f4be9e1f9219d
Lanoue <baptiste.lanoue@ensea.fr>
2019-12-03 22:42:00 +0100

    Modif CMakeList and add TestAdvancedAI add test engine

---
+++ test/shared/test_shared_AdvancedAI.cpp
@@ -0,0 +1,23 @@
+#include <boost/test/unit_test.hpp>
+#include <ai.h>
+#include <state.h>
+
+using namespace ::ai;
+using namespace ::state;
+
+
+BOOST_AUTO_TEST_CASE(TestAvancedAI){
+  engine::Engine engine;
+  engine::Engine* ptr_engine = &engine;
+  state::Coord coord = Coord(0,0);
+  state::Coord coord1 = Coord(1,1);
+  state::Coord& ref_coord = coord;
+  state::Coord& ref_coord1 = coord1;
+
+  AdvancedAI Test = AdvancedAI(0);
+  Test.play(ptr_engine);
+  Test.selectAnimal(ptr_engine);
+  //Test.selectAction(ptr_engine,ref_coord);
+  //Test.getDistance(ref_coord,ref_coord1);
+
+}
--- test/shared/test_shared_engine.cpp
+++ test/shared/test_shared_engine.cpp
@@ -1,8 +1,10 @@
 #include <boost/test/unit_test.hpp>
 #include <engine.h>
+#include <state.h>
 
 
 using namespace ::engine;
+using namespace ::state;
 
 BOOST_AUTO_TEST_CASE(TestStaticAssert)
 {
@@ -13,10 +15,69 @@ BOOST_AUTO_TEST_CASE(TestEngine)
 {
   Engine engine;
   Engine* ptr_engine = &engine;
-  std::vector<std::pair<state::Coord,engine::ActionID>> testAuth;
-  testAuth.push_back(make_pair(state::Coord(1,0),engine::ActionID(SHIFT)))
-  testAuth.push_back(make_pair(state::Coord(0,1),engine::ActionID(SHIFT)))
-  testAuth.push_back(make_pair(state::Coord(1,2),engine::ActionID(SHIFT)))
-  testAuth.push_back(make_pair(state::Coord(2,1),engine::ActionID(SHIFT)))
+  state::State& ref_state = engine.getState();
+  //std::vector<std::pair<state::Coord,engine::ActionID>> testAuth;
+  //testAuth.push_back(make_pair(state::Coord(1,0),engine::ActionID(SHIFT)))
+  //testAuth.push_back(make_pair(state::Coord(0,1),engine::ActionID(SHIFT)))
+  //testAuth.push_back(make_pair(state::Coord(1,2),engine::ActionID(SHIFT)))
+  //testAuth.push_back(make_pair(state::Coord(2,1),engine::ActionID(SHIFT)))
+  //engine.getState().getPlayer1().getAnimals()[7].setCoord(Coord(10,10));
+  //engine.getState().getPlayer1().getAnimals()[5].setCoord(Coord(5,5));
+  //engine.getState().getPlayer1().getAnimals()[0].setCoord(Coord(5,6));
+  //state::Coord& ref_Coordelephant = engine.getState().getPlayer1().getAnimals()[7].getCoord();
+
+  //engine.authorisedActions(ref_state,ref_Coordelephant);
+
+  for(int i =0;i<=7;i++){
+    Coord initCoord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(5,5));
+    state::Coord& ref_Coord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(initCoord);
+  }
+
+  for(int i =0;i<=7;i++){
+    Coord initCoord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(4,4));
+    state::Coord& ref_Coord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(1,6));
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(1,5));
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(10,5));
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(8,8));
+    engine.authorisedActions(ref_state,ref_Coord);
+
+
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(initCoord);
+  }
+
+  for(int i =0;i<=7;i++){
+    Coord initCoord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(i,2));
+    engine.getState().getPlayer2().getAnimals()[i].setCoord(Coord(i,3));
+    state::Coord& ref_Coord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(initCoord);
+    engine.getState().getPlayer2().getAnimals()[i].setCoord(initCoord);
+  }
+
+  for(int i =0;i<=7;i++){
+    Coord initCoord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(5,1));
+    engine.getState().getPlayer2().getAnimals()[i].setCoord(Coord(5,1));
+    state::Coord& ref_Coord = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(Coord(5,2));
+    engine.getState().getPlayer2().getAnimals()[i].setCoord(Coord(5,2));
+    state::Coord& ref_Coord2 = engine.getState().getPlayer1().getAnimals()[i].getCoord();
+    engine.authorisedActions(ref_state,ref_Coord);
+    engine.getState().getPlayer1().getAnimals()[i].setCoord(initCoord);
+    engine.getState().getPlayer2().getAnimals()[i].setCoord(initCoord);
+  }
+
+
 
 }
